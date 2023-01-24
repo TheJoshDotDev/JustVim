ecc8d8962443daf25fb518d00d6c4410a6556a5e
Rumone <rumoneandersonbiz@gmail.com>
2023-01-24 00:55:14 -0500

    remove unused code

--- lua/plugins/lsp/config/null-ls.lua
+++ lua/plugins/lsp/config/null-ls.lua
@@ -21,23 +21,6 @@ M.setup = function()
 				end,
 			}),
 		},
-		-- on_attach = function(current_client, bufnr)
-		-- 	if current_client.supports_method("textDocument/formatting") then
-		-- 		vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 			group = augroup,
-		-- 			buffer = bufnr,
-		-- 			callback = function()
-		-- 				vim.lsp.buf.format({
-		-- 					filter = function(client)
-		-- 						--  only use null-ls for formatting instead of lsp server
-		-- 						return client.name == "null-ls"
-		-- 					end,
-		-- 					bufnr = bufnr,
-		-- 				})
-		-- 			end,
-		-- 		})
-		-- 	end
-		-- end,
 	})
 
 	require("mason-null-ls").setup({
