732bb7a46c5a0f990a20e630a3efa492fab49c44
Rumone <rumone@peerledger.com>
2023-02-16 00:28:03 -0500

    refactor errors dir

--- lua/plugins/errors/init.lua
+++ lua/plugins/errors/init.lua
@@ -1,13 +1,11 @@
-local trouble_map = require("plugins.errors.config.keymaps").trouble_map
+local trouble_keys = require("plugins.errors.trouble.keys")
 return {
 	{
 		"folke/trouble.nvim",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
-		config = function()
-			require("trouble").setup()
-			trouble_map()
-		end,
+		keys = trouble_keys,
+		opts = {},
 	},
 }
---
+++ lua/plugins/errors/trouble/keys.lua
@@ -0,0 +1,5 @@
+return {
+	{ "<leader>xx", "<cmd>TroubleToggle<cr>" },
+	{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+	{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+}
