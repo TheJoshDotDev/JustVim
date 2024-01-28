63d70d343dadbf4798ed70b831a4b1846e5f14d9
Rumone <rumone@peerledger.com>
2024-01-28 17:29:31 -0500

    add support for nvim files at runtime

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -9,6 +9,11 @@ function G.setupLuaLs()
 				diagnostics = {
 					globals = { "vim" },
 				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
 			},
 		},
 	})
