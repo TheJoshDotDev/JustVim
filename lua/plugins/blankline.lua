4e2bf17d543d390c7bf9c1b6c924d63009a9e48e
Rumone <rumone@peerledger.com>
2024-01-31 19:55:00 -0500

    update blankline implementation

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -2,30 +2,22 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	opts = {},
-	conftig = function()
-		local blankline_ok, blankline = pcall(require, "indent_blankline")
+	config = function()
+		local blankline_ok, blankline = pcall(require, "ibl")
 		if not blankline_ok then
 			vim.notify("Indent blankline not found", vim.log.levels.ERROR)
 		end
+
+		vim.api.nvim_set_hl(0, "Blankline", { fg = "#4C4F69" })
+
 		blankline.setup({
 			indent = {
-				char = "│",
-				tab_char = "│",
-			},
-			exclude = {
-				filetypes = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
+				highlight = {
+					"Blankline",
 				},
+			},
+			scope = {
+				enabled = false,
 			},
 		})
 	end,
