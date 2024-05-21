0f029dfb2f99ab3eea5463e2a56b9642736e1e1d
Rumone <rumone@peerledger.com>
2024-05-21 18:31:08 -0500

    add conform formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				go = { "golines", "goimports-reviser", "gofumpt" },
+			},
+			format_after_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
